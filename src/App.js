b426e82feb9ec9f2fae0881831f681f2d23ecd45
Matheus Rocha <matheus_rocha98@outlook.com>
2019-11-04 14:45:14 -0200

    Finalizando curso

--- src/App.js
+++ src/App.js
@@ -1,49 +1,60 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
 
+import './App.css';
+import Header from './Header';
 import Tabela from './Tabela';
+import Formulario from './Formulario';
 
 class App extends Component {
-  
-    state = {
-      autores : [
-        {
-          nome: 'Paulo',
-          livro: 'React',
-          preco: '1000'
-        },
-        {
-          nome: 'Daniel',
-          livro: 'Java',
-          preco: '99'
-        },
-        {
-          nome: 'Marcos',
-          livro: 'Design',
-          preco: '150'
-        },
-        {
-          nome: 'Bruno',
-          livro: 'DevOps',
-          preco: '100'
-        }
-      ],
-    };
+
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      }
+    ],
+  };
 
   removeAutor = index => {
-    
+
     const { autores } = this.state;
-    
+
     this.setState(
       {
-        autores : autores.filter((autor, posAtual) => index !== posAtual )
+        autores: autores.filter((autor, posAtual) => index !== posAtual)
       }
     );
   }
 
+  escutadorDeSubmit = autor => this.setState({ autores: [...this.state.autores], autor });
+
   render() {
     return (
-      <Tabela autores={this.state.autores} removeAutor = { this.removeAutor } />  
+      <Fragment >
+        <Header />
+        <div className="container mb-10">
+          <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+          <Formulario escutadorDeSubmit={this.escutadorDeSubmit} />
+        </div>
+      </Fragment>
     );
   }
 }
