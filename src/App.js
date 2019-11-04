91b1bb8c1f12412a1a70612582058f88db6e16e9
Matheus Rocha <matheus_rocha98@outlook.com>
2019-11-04 11:44:01 -0200

    finalizando cap 3

--- src/App.js
+++ src/App.js
@@ -5,33 +5,45 @@ import Tabela from './Tabela';
 
 class App extends Component {
   
-    autores =  [
-      {
-        nome: 'Paulo',
-        livro: 'React',
-        preco: '1000'
-      },
-      {
-        nome: 'Daniel',
-        livro: 'Java',
-        preco: '99'
-      },
-      {
-        nome: 'Marcos',
-        livro: 'Design',
-        preco: '150'
-      },
-      {
-        nome: 'Bruno',
-        livro: 'DevOps',
-        preco: '100'
-      }
-    ];
+    state = {
+      autores : [
+        {
+          nome: 'Paulo',
+          livro: 'React',
+          preco: '1000'
+        },
+        {
+          nome: 'Daniel',
+          livro: 'Java',
+          preco: '99'
+        },
+        {
+          nome: 'Marcos',
+          livro: 'Design',
+          preco: '150'
+        },
+        {
+          nome: 'Bruno',
+          livro: 'DevOps',
+          preco: '100'
+        }
+      ],
+    };
 
+  removeAutor = index => {
+    
+    const { autores } = this.state;
+    
+    this.setState(
+      {
+        autores : autores.filter((autor, posAtual) => index !== posAtual )
+      }
+    );
+  }
 
   render() {
     return (
-      <Tabela autores={this.autores}/>  
+      <Tabela autores={this.state.autores} removeAutor = { this.removeAutor } />  
     );
   }
 }
