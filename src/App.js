6d39b32d9a16f45e3f73127f926c97aa295bf448
Matheus Rocha <matheus_rocha98@outlook.com>
2019-11-04 11:20:18 -0200

    Finalizando capitulos 1 e 2 (agora sim)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+import Tabela from './Tabela';
+
+class App extends Component {
+  
+    autores =  [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      }
+    ];
+
+
+  render() {
+    return (
+      <Tabela autores={this.autores}/>  
+    );
+  }
 }
 
 export default App;
